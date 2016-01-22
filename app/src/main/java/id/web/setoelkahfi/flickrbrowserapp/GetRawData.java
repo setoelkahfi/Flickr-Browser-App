76b6e25ea84b516714f79d048169d9f8dac6d5d3
Seto El Kahfi <setoelkahfi@gmail.com>
2016-01-22 18:13:46 +0700

    Initial commit

---
+++ app/src/main/java/id/web/setoelkahfi/flickrbrowserapp/GetRawData.java
@@ -0,0 +1,120 @@
+package id.web.setoelkahfi.flickrbrowserapp;
+
+import android.os.AsyncTask;
+import android.test.RenamingDelegatingContext;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Eldin on 1/22/2016.
+ * id.web.setoelkahfi.flickrbrowserapp
+ */
+
+enum DownloadStatus { IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK }
+
+public class GetRawData {
+    private String LOG_TAG = GetRawData.class.getSimpleName();
+    private String mRawURL;
+    private String mData;
+    private DownloadStatus mDownloadStatus;
+
+    public GetRawData(String mRawURL) {
+        this.mRawURL = mRawURL;
+        this.mDownloadStatus = DownloadStatus.IDLE;
+    }
+
+    public void reset() {
+        this.mDownloadStatus = DownloadStatus.IDLE;
+        this.mRawURL = null;
+        this.mData = null;
+    }
+
+    public String getmData() {
+        return mData;
+    }
+
+    public DownloadStatus getmDownloadStatus() {
+        return mDownloadStatus;
+    }
+
+    public void execute() {
+        this.mDownloadStatus = DownloadStatus.PROCESSING;
+        DownloadRawData downloadRawData = new DownloadRawData();
+        downloadRawData.execute(mRawURL);
+    }
+
+    public class DownloadRawData extends AsyncTask<String, Void, String> {
+
+        protected void onPostExecute(String webData) {
+            mData = webData;
+            Log.v(LOG_TAG, "Data returned was: " + mData);
+            if (mData == null) {
+                if (mRawURL == null) {
+                    mDownloadStatus = DownloadStatus.NOT_INITIALIZED;
+                } else {
+                    mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
+                }
+            } else {
+                // Success
+                mDownloadStatus = DownloadStatus.OK;
+            }
+        }
+
+        protected String doInBackground(String... params) {
+            HttpURLConnection httpURLConnection = null;
+            BufferedReader bufferedReader = null;
+
+            if (params == null) {
+                return null;
+            }
+
+            try {
+                URL url = new URL(params[0]);
+
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.connect();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                if (inputStream == null) {
+                    return null;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                return buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                return null;
+            } finally {
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+    }
+}
